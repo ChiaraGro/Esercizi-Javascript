27c7b27cdbf1c56c260cf4230526964bcb8576d9
Chiara Grottini <chiaragrottini@gmail.com>
2021-09-28 22:22:58 +0200

    es 41/43

---
+++ Es.43-error handling/exercise-3.js
@@ -0,0 +1,61 @@
+class BankAccount {
+  #amount = 0;
+
+  constructor(initialAmount) {
+    this.#amount = initialAmount;
+  }
+
+  deposit(amount) {
+    if (amount < 0) {
+      throw new NegativeAmountError('The amount provided cannot be negative'); // Use custom Error class
+    }
+
+    this.#amount += amount;
+  }
+
+  withdraw(amount) {
+    if (amount < 0) {
+      throw new NegativeAmountError('The amount provided cannot be negative'); // Use custom Error class
+    }
+
+    if (this.#amount < amount) {
+      throw new WithdrawNotPermittedError('You cannot withdraw more than account balance'); // Use custom Error class
+    }
+
+    this.#amount -= amount;
+  }
+
+  view() {
+    console.log(this.#amount);
+  }
+}
+
+class WithdrawNotPermittedError extends Error{
+  constructor(){
+    super();
+    this.name = 'WithdrawNotPermittedError';
+    this.message = 'You cannot withdraw more than account balance';
+  }
+
+}
+
+
+class NegativeAmountError extends Error{
+  constructor(){
+    super();
+    this.name = 'NegativeAmountError';
+    this.message = 'The amount provided cannot be negative'
+  }
+}
+
+
+
+try {
+  const bankAccount = new BankAccount(1000);
+  bankAccount.deposit(500);
+  bankAccount.deposit(200);
+  bankAccount.withdraw(10000);
+  bankAccount.view();
+} catch (e) {
+  console.log('Something went wrong during bank account operations');
+}
